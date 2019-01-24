578ba72365f45c615eec67b4f21f8412d983c74a
mksinghania <31793344+mksinghania@users.noreply.github.com>
2019-01-24 23:32:21 +0530

    add packages
    it will help

--- commands.R
+++ commands.R
@@ -14,5 +14,10 @@ typeof(variable_name)
 mode(variable_name)
 class(variable_name)
 
-
+installed.packages()  # to show all installed packages
+install.package("dplyr")  # dplyr is new package to install
+library("dplyr")  # after install we have to load the package
+?name_of _any_value # to show description
+??name_of _any_value # to show description in better way topic vise
+ 
 I CAN'T TELL YOU MORE BECAUSE TERMS AND CONDITIONS APPLY HERE YOU WILL NOT UNDERSTAND
