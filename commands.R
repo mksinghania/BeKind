d4c2264eba5c9f2b705f1629dfdd81d968fbdb79
mksinghania <31793344+mksinghania@users.noreply.github.com>
2019-01-24 23:04:33 +0530

    add data frame

--- commands.R
+++ commands.R
@@ -5,8 +5,8 @@ data()   # to show all by default data set available
 lst<-list("mk","mr")
 pts <- list(x = cars[,1], y = cars[,2])
 
-
-
+c<-data.frame(x=c(1,2),y=c(3,4))
+c
 
 
 I CAN'T TELL YOU MORE BECAUSE TERMS AND CONDITIONS APPLY HERE YOU WILL NOT UNDERSTAND
