9c7fa962015a74ead00fcb2d0a32ce0fb2758f66
mksinghania <31793344+mksinghania@users.noreply.github.com>
2019-01-24 23:02:20 +0530

    add list

--- commands.R
+++ commands.R
@@ -2,4 +2,11 @@ data()   # to show all by default data set available
 .lib.paths()  # to show library path
 .library  # same as above
 
+lst<-list("mk","mr")
+pts <- list(x = cars[,1], y = cars[,2])
+
+
+
+
+
 I CAN'T TELL YOU MORE BECAUSE TERMS AND CONDITIONS APPLY HERE YOU WILL NOT UNDERSTAND
