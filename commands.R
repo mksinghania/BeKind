f6d46102de2c21cf31c00e2bfbf1662891020ab1
mksinghania <31793344+mksinghania@users.noreply.github.com>
2019-01-24 22:42:09 +0530

    check once

---
+++ commands.R
@@ -0,0 +1,5 @@
+data()   # to show all by default data set available
+.lib.paths()  # to show library path
+.library  # same as above
+
+I CAN'T TELL YOU MORE BECAUSE TERMS AND CONDITIONS APPLY HERE YOU WILL NOT UNDERSTAND
