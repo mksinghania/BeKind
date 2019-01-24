aa09ff280dc712d41cb4b90a9f13b4d1b8e60b52
mksinghania <31793344+mksinghania@users.noreply.github.com>
2019-01-24 23:25:41 +0530

    show data type

--- commands.R
+++ commands.R
@@ -8,5 +8,11 @@ pts <- list(x = cars[,1], y = cars[,2])
 c<-data.frame(x=c(1,2),y=c(3,4))
 c
 
+#to show type and class 
+
+typeof(variable_name)
+mode(variable_name)
+class(variable_name)
+
 
 I CAN'T TELL YOU MORE BECAUSE TERMS AND CONDITIONS APPLY HERE YOU WILL NOT UNDERSTAND
